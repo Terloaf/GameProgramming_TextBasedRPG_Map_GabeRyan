89180d8577270242eb408ddecfd66502cfb69d90
Terloaf <greenyryan2501@gmail.com>
2025-11-04 14:27:50 -0400

    Update Program.cs

--- GameProgramming_TextBasedRPG_Map_GabeRyan/Program.cs
+++ GameProgramming_TextBasedRPG_Map_GabeRyan/Program.cs
@@ -33,8 +33,6 @@ namespace GameProgramming_TextBasedRPG_Map_GabeRyan
 
 
         }
-
-
         static void DisplayMap()
         {
             for (int i = 0; i < map.GetLength(0); i++)
@@ -42,14 +40,98 @@ namespace GameProgramming_TextBasedRPG_Map_GabeRyan
 
                 for (int j = 0; j < map.GetLength(1); j++)
                 {
-                    
+
                     Console.Write(map[i, j]);
+
+
                 }
 
+                Console.WriteLine();
+            }
+            Console.Write("\n");
+        }
 
+        static void DisplayMap(int scale)
+        {
+
+            if (scale == 1)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+                        Console.Write(map[i, j]);
+
+
+                    }
+
+                    Console.WriteLine();
+                }
                 Console.Write("\n");
             }
+            if(scale == 2)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
 
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+                        Console.Write(map[i, j]);
+
+                        Console.Write(map[i, j]);
+
+
+                    }
+
+                    Console.WriteLine();
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+                        Console.Write(map[i, j]);
+
+                        Console.Write(map[i, j]);
+
+
+                    }
+                    Console.Write("\n");
+                }
+            }
+
+            if(scale == 3)
+            {
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+                        Console.Write(map[i, j]);
+
+                        Console.Write(map[i, j]);
+
+                        Console.Write(map[i, j]);
+
+
+                    }
+
+                    Console.WriteLine();
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+
+                        Console.Write(map[i, j]);
+
+                        Console.Write(map[i, j]);
+
+                        Console.Write(map[i, j]);
+
+
+                    }
+                    Console.Write("\n");
+                }
+            }
 
 
         }
